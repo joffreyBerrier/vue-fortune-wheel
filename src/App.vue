7505ac8db4cac47a4041b89eb9c62f3cf34df6f5
joffrey <joffrey.berrier@gmail.com>
2021-06-14 18:40:25 +0200

    feat(wheel): create a basic wheel components

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap">
+    <Wheel />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Wheel from './components/Wheel.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Wheel
+  }
+})
+</script>
+
+<style>
+.wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
